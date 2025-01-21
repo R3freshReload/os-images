24a72966038b011225584a7da7408d390928d251
Daniel Seifert <danielseifert020@gmail.com>
2025-01-22 00:48:41 +0100

    refactor

--- rbpi/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -eux
-docker build -t registry.blitzid.org/rbpi-os .
-#docker push registry.blitzid.org/rbpi-os
-docker run --rm -ti -v /var/run/docker.sock:/var/run/docker.sock --entrypoint /bin/sh -v $(pwd)/build:/output ghcr.io/rancher/elemental-toolkit/elemental-cli:v2.2.0 -c "elemental build-disk --squash-no-compression --system registry.blitzid.org/rbpi-os --config-dir /config --local --output /build --expandable --debug -n rbpi && mv /build/rbpi.raw /output/"
-qemu-img convert -O qcow2 build/rbpi.raw build/rbpi.qcow2 
-qemu-img resize build/rbpi.qcow2 20G 
