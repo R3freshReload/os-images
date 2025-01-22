16b52cbdc98aeee02b6c6c36d2e9ba39ca05266c
Daniel Seifert <danielseifert020@gmail.com>
2025-01-22 11:10:51 +0100

    trigger os image workflow

--- examples/rbpi/build.sh
+++ examples/rbpi/build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -eux
+
 docker build -t example-rbpi-os .
 docker run --rm -ti -v /var/run/docker.sock:/var/run/docker.sock --entrypoint /bin/sh -v $(pwd)/build:/output ghcr.io/rancher/elemental-toolkit/elemental-cli:v2.2.0 -c "elemental build-disk --squash-no-compression --system example-rbpi-os --config-dir /config --local --output /build --expandable --debug -n rbpi && mv /build/rbpi.raw /output/"
 qemu-img convert -O qcow2 build/rbpi.raw build/rbpi.qcow2 
